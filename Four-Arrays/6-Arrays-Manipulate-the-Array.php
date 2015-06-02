5540318b9e31c41633a24f9b10d2472ceb421117
Jose Iriarte <josemariairiarte@gmail.com>
2015-06-02 14:24:37 -0300

    Arrays (Part 2)

--- Four-Arrays/6-Arrays-Manipulate-the-Array.php
+++ Four-Arrays/6-Arrays-Manipulate-the-Array.php
@@ -34,13 +34,116 @@ if you like:
 <?php 
 
 echo "My Solution.<br/>";
+// initalize the array
+$aprilTemperatures = array(68, 70, 72, 58, 60, 79, 82, 73, 75, 77, 73, 58, 63, 79, 78,
+68, 72, 73, 80, 79, 68, 72, 75, 77, 73, 78, 82, 85, 89, 83);
 
+// create copy of the array for five min temperatures
+$aprilTemperatures2 = $aprilTemperatures;
+
+
+//find the average temperature:
+$count = count($aprilTemperatures);
+ 
+$total = 0;
+foreach ($aprilTemperatures as $temperature){
+  $total += $temperature;
+}
+ 
+//Calculate average.
+echo "</br> The average temperature is: ";
+$averageTemperature = round($total / $count);
+echo $averageTemperature . "&deg";
+
+
+// find the highest temperature:
+echo "</br> The highest temperature is: ";
+$averageHighTemperature = max($aprilTemperatures);
+echo $averageHighTemperature . "&deg";
+
+
+// find the five warmest temperatures
+asort($aprilTemperatures);
+
+$t1 = array_pop($aprilTemperatures);
+$t2 = array_pop($aprilTemperatures);
+$t3 = array_pop($aprilTemperatures);
+$t4 = array_pop($aprilTemperatures);
+$t5 = array_pop($aprilTemperatures);
+
+
+echo "</br> The five warmest temperatures are: ";
+$fiveWarmestTemperatures = array($t1, $t2, $t3, $t4, $t5);
+print_r($fiveWarmestTemperatures);
+foreach ($fiveWarmestTemperatures as $Temperature) {
+	echo "</br>" . $Temperature . "&deg";
+}
+
+
+// find the five coolest temperatures
+arsort($aprilTemperatures2);
+$i = 0;
+$fiveCoolestTemperatures = array();
+for ( $i = 0; $i <= 5; $i++) {
+	$fiveCoolestTemperatures[$i] = array_pop($aprilTemperatures2);
+}
+echo "</br> The five coolest temperatures are:";
+print_r($fiveCoolestTemperatures);
+foreach ($fiveCoolestTemperatures as $Temperature) {
+	echo "</br>" . $Temperature . "&deg";
+}
+
+//}
+// $fiveCoolestTemperatures = 
 
 // Official Solution
-echo "<br/>";
+echo "<br/></br>";
 echo "Official Solution.<br/>";
-
-
-?> 
+?>
+<h2>High Temperatures for Spring Month</h2>
+ 
+<?php
+//Create an array of 30 Fahrenheit high temperatures for a spring month.
+$highTemps = array(
+  68, 70, 72, 58, 60, 79, 82, 73, 75, 77, 73, 58, 63, 79, 78, 
+  68, 72, 73, 80, 79, 68, 72, 75, 77, 73, 78, 82, 85, 89, 83
+);
+ 
+//Get number of temps.
+$count = count($highTemps);
+ 
+//Get a total of all temps.
+$total = 0;
+foreach ($highTemps as $h){
+  $total += $h;
+}
+ 
+//Calculate average.
+$avg = round($total / $count);
+ 
+//Send data to the browser. &deg; is the ASCII code for the degree sign.
+echo "<p>The average high temperature for the month was $avg &deg;F.</p>\n";
+ 
+//Sort the temps and get the top and bottom five. 
+//Use rsort to produce a descending sort.
+rsort($highTemps);
+//Pull out the top 5 temps.
+$topTemps = array_slice($highTemps, 0, 5);
+echo "<p>The warmest five high temperatures were: <br />\n";
+foreach($topTemps as $t){
+  echo "$t &deg;F <br/> \n";
+}
+echo "</p>";
+   
+//Pull out the bottom five temps.
+$lowTemps = array_slice($highTemps, -5, 5);
+echo "<p>The coolest five high temperatures were: <br/>\n";
+foreach($lowTemps as $l){
+  echo "$l &deg;F <br/> \n";
+}
+echo "</p>";
+ 
+?>
+  
 </body>
 </html>
--- Four-Arrays/7-Arrays-Multi-Dimensional-Array.php
+++ Four-Arrays/7-Arrays-Multi-Dimensional-Array.php
@@ -39,6 +39,13 @@ table {border-collapse: collapse; border: 1px solid black;}
   <meta charset="utf-8">
 
   <title>Exercise 21</title>
+<style type="text/css">
+td, th {width: 8em; border: 1px solid black; padding-left: 4px;}
+th {text-align:center;}
+table {border-collapse: collapse; border: 1px solid black;}
+</style>
+
+
  </head>
 
 <body>
@@ -46,12 +53,75 @@ table {border-collapse: collapse; border: 1px solid black;}
 
 echo "My Solution.<br/>";
 
+$multiCity = array(
+	array("City", "Country", "Continent"),
+	array("Tokyo", "Japan", "Asia"),
+	array("Mexico City", "Mexico", "North America"),
+	array("New York City", "USA", "North America"),
+	array("Mumbai", "India", "Asia"),
+	array("Seoul", "South Korea", "Asia"),
+	array("Shanghai", "China", "Asia"),
+	array("Lagos", "Nigeria", "Africa"), 
+	array("Buenos Aires", "Argentina", "South America"),
+	array("Cairo", "Egypt", "Africa"),
+	array("London", "UK", "Europe")
+	);
+echo "<table>
+	<thead>
+		<tr>
+			<th>" . $multiCity[0][0] . "</th>
+			<th>" . $multiCity[0][1] . "</th>
+			<th>" . $multiCity[0][2] . "</th>
+		</tr>
+	</thead>
+	<tbody>";
+		$count = count($multiCity);
+		for ($i=1; $i<=$count-1; $i++) {
+			echo "<tr>";
+			foreach ($multiCity[$i] as $data) {
+				echo "<td>" . $data. "</td>";
+			} 
+			echo "</tr>";
+		}
+echo 	"</tbody>
+</table>";
 
 // Official Solution
-echo "<br/>";
-echo "Official Solution.<br/>";
+echo "</br>";
+echo "Official Solution";
+echo "</br>";
+?>
 
-
-?> 
+<h2>City Table<br /></h2>
+ 
+<table>
+<thead>
+<tr>
+<th>
+<?php
+echo $multiCity[0][0] ."</th>\n<th>";
+echo $multiCity[0][1] ."</th>\n<th>";
+echo $multiCity[0][2] ."</th>\n";
+?>
+</tr>
+</thead>
+ 
+<?php
+//Find the number of rows in the array. Using this variable in the for loop
+//instead of the exact number gives you the option of adding to the array 
+//at a later date without changing the for loop. 
+$num = count($multiCity); 
+//For loops begin with "0" to iterate over an entire array. In this case, 
+//you need to begin with the second item in the array and must start with "1".
+for ($row=1; $row<$num; $row++){
+  echo "<tr>\n";
+  foreach ($multiCity[$row] as $value){
+    echo "<td>$value</td>\n";
+    }
+   echo "</tr>\n";  
+}
+?>
+ 
+</table> 
 </body>
 </html>
